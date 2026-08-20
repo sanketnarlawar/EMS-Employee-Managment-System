0d1ba6e36d6e0c8f71f411b3960ed53bc2584ddc
sanketnarlawar <sanketnarlawar@gmail.com>
2026-08-21 01:44:43 +0530

    added the sidebar functionality and also modified some data

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -24,9 +24,9 @@ export const dummyEmployeeDashboardData = {
 
 export const dummyProfileData = {
     _id: "69b411e6f8a807df391d7b13",
-    firstName: "John",
-    lastName: "Doe",
-    email: "johndoe@example.com",
+    firstName: "Sanket",
+    lastName: "Narlawar",
+    email: "sanket@example.com",
     image: null,
 };
 
